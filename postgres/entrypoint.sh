93d31dd3f93b6aa5ee6c589c81011041edab8407
Ruslan Khozinov <rkhozinov@mirantis.com>
2016-12-26 13:39:08 +0000

    test-env and selenium based on docker hub images
    - test-env based on node image
    - selenium based on bunch of slelinum images' sources

--- postgres/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-i#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE ROLE nailgun WITH LOGIN PASSWORD 'nailgun;
-    CREATE USER nailgun;
-    CREATE DATABASE nailgun;
-    GRANT ALL PRIVILEGES ON DATABASE nailgun TO nailgun;
-EOSQL
