75598024a25b47a361c0d54ffb48d1060e00480d
Ruslan Khozinov <rkhozinov@mirantis.com>
2017-01-10 14:42:18 +0000

    refactoring

--- selenium/entrypoint.sh
+++ selenium/entrypoint.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
 
-cat << CONFIG > /opt/selenium/config.json
-{
-  "capabilities": [
-    {
-      "browserName": "firefox",
-      "maxInstances": ${NODE_MAX_INSTANCES:-1},
-      "seleniumProtocol": "WebDriver"
-    }
-  ],
-  "proxy": "org.openqa.grid.selenium.proxy.DefaultRemoteProxy",
-  "maxSession": ${NODE_MAX_SESSION:-1},
-  "port": ${SELENIUM_SERVER_PORT:-4444},
-  "register": true,
-  "registerCycle": ${NODE_REGISTER_CYCLE:-1}
-}
-CONFIG
-
-export GEOMETRY="${SCREEN_WIDTH:-1920}""x""${SCREEN_HEIGHT:-1080}""x""${SCREEN_DEPTH:-24}"
-
-if [ ! -e /opt/selenium/config.json ]; then
-  echo No Selenium Node configuration file, the node-base image is not intended to be run directly. 1>&2
-  exit 1
-fi
+export GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
 
 function shutdown {
   kill -s SIGTERM $NODE_PID
@@ -37,20 +15,10 @@ SERVERNUM=$(echo $DISPLAY | sed -r -e 's/([^:]+)?:([0-9]+)(\.[0-9]+)?/\2/')
 
 rm -f /tmp/.X*lock
 
-# FROM StandaloneFirefox
-
-#xvfb-run -n $SERVERNUM --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
-#  java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
-#    -role node \
-#    -hub http://$HUB_PORT_4444_TCP_ADDR:$HUB_PORT_4444_TCP_PORT/grid/register \
-#    -nodeConfig /opt/selenium/config.json \
-#    ${SE_OPTS} &
-#NODE_PID=$!
-
-xvfb-run -n ${SERVERNUM:?} --server-args="-screen 0 ${GEOMETRY:?} -ac +extension RANDR" \
+xvfb-run -n $SERVERNUM --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
   java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
   ${SE_OPTS} &
 NODE_PID=$!
 
 trap shutdown SIGTERM SIGINT
-wait ${NODE_PID:?}
+wait $NODE_PID
