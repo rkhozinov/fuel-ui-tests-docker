10a5e5c4663ca151697fa812670f9d9ea8b6b0fa
Ekaterina Khomyakova <ekhomyakova@tpi96.bud.mirantis.net>
2017-03-09 14:31:23 +0000

    Backup

--- node/entrypoint.sh
+++ node/entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-cd /fuel-web/nailgun
-sudo pip install --allow-all-external -r test-requirements.txt
-
 cd /fuel-ui
-npm install > npm_install.log
+#npm install > npm_install.log
 
-pip uninstall -y tox
-pip install tox
+#pip uninstall -y tox
+#pip install tox
 
-/etc/init.d/postgresql start && bash run_ui_func_tests.sh static/tests/functional/nightly/test_cluster_workflows.js
+/etc/init.d/postgresql start
+bash run_ui_func_tests.sh static/tests/functional/nightly/test_cluster_workflows.js
